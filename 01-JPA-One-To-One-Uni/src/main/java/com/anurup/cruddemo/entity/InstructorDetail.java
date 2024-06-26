a442af013329fb54df0419adc8d089aac73623ce
Anurup-Dey <47382133+Anurup-Dey@users.noreply.github.com>
2024-06-26 18:48:51 +0530

    Add files via upload
    Initial Commit

---
+++ 01-JPA-One-To-One-Uni/src/main/java/com/anurup/cruddemo/entity/InstructorDetail.java
@@ -0,0 +1,24 @@
+package com.anurup.cruddemo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "instructor_detail")
+public class InstructorDetail {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "youtube_channel")
+	private String youtubeChannel;
+
+	@Column(name = "hobby")
+	private String hobby;
+}
---
+++ 01-JPA-One-To-One-Uni/src/test/java/com/anurup/__JPA_One_To_One_Uni/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anurup.__JPA_One_To_One_Uni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
